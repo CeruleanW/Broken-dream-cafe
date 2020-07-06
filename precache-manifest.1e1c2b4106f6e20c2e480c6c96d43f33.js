d38089dcdc485c74bf2e07990e6602d5c30b30b2
CeruleanW <yangyi6463@gmail.com>
2020-07-06 08:11:51 -0400

    Updates

---
+++ precache-manifest.1e1c2b4106f6e20c2e480c6c96d43f33.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6ada376c7f0120eee0de4dd274ee7c",
+    "url": "/Broken-dream-cafe/index.html"
+  },
+  {
+    "revision": "5c0f82094c27ece97b75",
+    "url": "/Broken-dream-cafe/static/css/2.685706c2.chunk.css"
+  },
+  {
+    "revision": "b649120823b4bae62a15",
+    "url": "/Broken-dream-cafe/static/css/main.0ca44f15.chunk.css"
+  },
+  {
+    "revision": "5c0f82094c27ece97b75",
+    "url": "/Broken-dream-cafe/static/js/2.7051c4ee.chunk.js"
+  },
+  {
+    "revision": "7e01d19aff7748620441570548d24305",
+    "url": "/Broken-dream-cafe/static/js/2.7051c4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b649120823b4bae62a15",
+    "url": "/Broken-dream-cafe/static/js/main.cc049e8a.chunk.js"
+  },
+  {
+    "revision": "7cb91ef7cec9a693d057",
+    "url": "/Broken-dream-cafe/static/js/runtime-main.cdafaf51.js"
+  },
+  {
+    "revision": "2c8a000fda9f1ee411d8dad1dac3a480",
+    "url": "/Broken-dream-cafe/static/media/StockSnap_DW4W4JKS9I-large.2c8a000f.jpg"
+  },
+  {
+    "revision": "63021c646337c3b623b2be58404dbd7c",
+    "url": "/Broken-dream-cafe/static/media/StockSnap_UKPXEXD4TT.63021c64.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Broken-dream-cafe/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Broken-dream-cafe/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Broken-dream-cafe/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Broken-dream-cafe/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Broken-dream-cafe/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Broken-dream-cafe/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Broken-dream-cafe/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Broken-dream-cafe/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Broken-dream-cafe/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Broken-dream-cafe/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Broken-dream-cafe/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "abdac7760e2c83b05d89e3f3a3d923a2",
+    "url": "/Broken-dream-cafe/static/media/octogon.abdac776.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Broken-dream-cafe/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Broken-dream-cafe/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Broken-dream-cafe/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Broken-dream-cafe/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Broken-dream-cafe/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
--- src/index.js
+++
@@ -1,3 +0,0 @@
-console.log("Testing")
-// appending each new element to div#content once the page is first loaded
-// initial page-load function
